7bfd0270e14641a024a2a6614c969052c6e48761
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-11-02 06:07:48 -0700

    Implementation of Binary search in Python

---
+++ binarysearch.py
@@ -0,0 +1,21 @@
+
+def BubbleSort(array):
+    for i in range(len(array)):
+
+    for j in range(0, len(array) - i - 1):
+
+
+      if array[j] > array[j + 1]:
+
+
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+
+
+data = [-4, 25, 10, 0, -7]
+
+BubbleSort(data)
+
+print('Sorted Array in Ascending Order:')
+print(data)
