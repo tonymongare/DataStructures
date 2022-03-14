b334b8e3eb08fd5b5506860a857ea8bfed9cfae3
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-14 11:49:53 -0700

    Update double_ended_queue.py

--- double_ended_queue.py
+++ double_ended_queue.py
@@ -31,7 +31,7 @@ class Deque:
         return self.items == []
         
     def addRear(self, item):
-        return self.items.append(item
+        return self.items.append(ite
         
     def addFront(self, item):
         self.items.insert(0, item)
