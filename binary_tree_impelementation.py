b2f6e175f19c80a898f277971eaf20b70e4446ab
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-26 22:04:10 -0700

    Create binary_tree_impelementation.py
    The implementation and traversal of binary tree in Python

---
+++ binary_tree_impelementation.py
@@ -0,0 +1,38 @@
+class Node:
+    
+    def __init__(self, item):
+        self.right = None
+        self.left = None
+        self.data = item
+        
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print(self.data, end="")
+        
+        if self.right:
+            self.righ.iorder()
+            
+    def preorder(self):
+        print(self.data, end="")
+        if self.left:
+            self.left.preorder();
+        if self.right:
+                self.right.preorder()
+                
+    def postorder(self):
+        if self.left:
+            self.left.postorder
+        if self.right:
+            self.right.postorder
+        print(self.data, end="")
+        
+root = Node(23)
+root.left = Node(24)
+root.right = Node(25)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+
+                
